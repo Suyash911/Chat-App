21154ebd93971d62491ed83938bc0d5819cf97ef
Komal Vaishnav <vaishnavkomal60@gmail.com>
2025-08-14 21:12:55 +0530

    Successfully Done 1 hr

---
+++ Chat_app/src/Components/RightSidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <>
+    <div>
+        <h1>Rightsidebar</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default RightSidebar
